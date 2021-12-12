9465ff35f14ad13caf25cf0071831984e1afc4c1
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2021-12-12 02:05:43 -0700

    Refactor and update to Vanta background

--- build.js
+++ build.js
@@ -1,62 +1,35 @@
 const fs = require("fs");
-const ncp = require('ncp').ncp;
 const path = require("path");
 const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
+const {hideBin} = require('yargs/helpers');
 const babelify = require("babelify");
 const watchify = require('watchify');
 const browserify = require("browserify");
 const browserifyShim = require("browserify-shim");
+const esmify = require('esmify');
 
 const argv = yargs(hideBin(process.argv)).argv;
 
 // START Build options/paths
 
 const srcDir = "src";
-const buildDir = "build";
-const copyFileToBuildDirPaths = [
-    path.join(srcDir, "index.html"),
-    path.join(srcDir, "main.css"),
-];
-const copyDirectoryToBuildDirPaths = [
-    "assets"
-];
-const mainJSPath = path.join(srcDir, "js/main.js");
-const bundleJSPath = path.join(buildDir, "bundle.js");
+const distDir = "public/js/";
+const mainJSPath = path.join(srcDir, "Main.js");
+const bundleJSPath = path.join(distDir, "bundle.js");
 
 // END Build options/paths
 
-// Delete 'buildDir'
-if (fs.existsSync(buildDir)) {
-    console.info("Deleting contents of build directory: %s", buildDir);
-    fs.rmdirSync(buildDir, { recursive: true });
-    fs.mkdirSync(buildDir)
+// Delete 'distDir'
+if (fs.existsSync(distDir)) {
+    console.info("Deleting contents of build directory: %s", distDir);
+    fs.rmdirSync(distDir, {recursive: true});
 }
 
-// Copy files defined in 'copyFileToBuildDirPaths'
-for (let pathIndex = 0; pathIndex < copyFileToBuildDirPaths.length; pathIndex++) {
-    let fromPath = copyFileToBuildDirPaths[pathIndex];
-    let toPath = path.join(buildDir, path.basename(fromPath));
-
-    console.info("Copying file %s to %s", fromPath, toPath);
-
-    fs.copyFileSync(fromPath, toPath);
-}
-
-// Copy directories recursively defined in 'copyDirectoryToBuildDirPaths'
-for (let pathIndex = 0; pathIndex < copyDirectoryToBuildDirPaths.length; pathIndex++) {
-    let fromPath = copyDirectoryToBuildDirPaths[pathIndex];
-    let toPath = path.join(buildDir, path.basename(fromPath));
-
-    console.info("Copying directory %s to %s", fromPath, toPath);
-
-    ncp.ncp(fromPath, toPath, (err) => {
-        if (err) throw err;
-    });
-}
+fs.mkdirSync(distDir)
 
 console.info("Browserifying...");
 let iBrowserify = browserify(mainJSPath, {cache: {}, packageCache: {}});
+iBrowserify.plugin(esmify)
 applyTransforms(iBrowserify);
 bundle();
 
@@ -64,6 +37,7 @@ if (argv.watchify) {
     console.info("Watching JS file: " + mainJSPath);
 
     iBrowserify.plugin(watchify);
+    iBrowserify.plugin(esmify)
     iBrowserify.on("update", bundle);
     iBrowserify.on('log', function (msg) {
         console.log(msg)
---
+++ src/Main.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+
+$(window).on('load', function () {
+    $('#background').animate({opacity: 1}, 1500);
+    addFadeInAndEaseDelay('#name', 1500, 3000);
+    addFadeInAndEaseDelay('#link-github', 3500, 3000);
+    addFadeInAndEaseDelay('#link-linkedin', 4000, 3000);
+});
+
+function addFadeInAndEaseDelay(jquerySelector, delayMillis, fadeInMillis) {
+    setTimeout(function () {
+        $(jquerySelector).animate({opacity: 1}, fadeInMillis).addClass('ease-animation');
+    }, delayMillis);
+}
+
+VANTA.TOPOLOGY({
+    el: "#background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xfc1212,
+    backgroundColor: 0x0
+})
